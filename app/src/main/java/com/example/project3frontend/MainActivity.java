a3b8bcb10eaab39cdbcccdf7dac87a21517eee72
James Campbell <jscampbell43@gmail.com>
2021-11-11 13:54:00 -0800

    Added Network of Buttons between activities Added 2 buttons to Profile to get to New Request and Open Projects Added Back button to return to Profile from New Request and Open Projects Added initial layout for Open Projects Activity

--- app/src/main/java/com/example/project3frontend/MainActivity.java
+++ app/src/main/java/com/example/project3frontend/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.project3frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Layout pieces
+    private TextView textViewProfileWelcome;
+    private static final String TAG = "MainActivity";
+    private Button buttonRequest;
+    private Button buttonFind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Attach layout
+        textViewProfileWelcome = findViewById(R.id.textViewProfileWelcome);
+        buttonRequest = findViewById(R.id.buttonRequest);
+        buttonFind = findViewById(R.id.buttonFind);
+
+        // Intent Factory for buttons
+        IntentFactory factory = new IntentFactory();
+
+        buttonRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = factory.getIntent(MainActivity.this, NewRequestActivity.class);
+                    startActivity(intent);
+            }
+
+        });
+
+        buttonFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = factory.getIntent(MainActivity.this, OpenProjectsActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/project3frontend/OpenProjectsActivity.java
+++ app/src/main/java/com/example/project3frontend/OpenProjectsActivity.java
@@ -2,13 +2,43 @@ package com.example.project3frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class OpenProjectsActivity extends AppCompatActivity {
+
+    // Layout pieces
+    private Button buttonBackToProfile;
+    private TextView textViewOpenProjectsWelcome;
+    private EditText editTextSearchProject;
+    private Button buttonSearchProject;
+    private static final String TAG = "NewRequestActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_projects);
+
+        // Attach layout
+        textViewOpenProjectsWelcome = findViewById(R.id.textViewOpenProjectsWelcome);
+        editTextSearchProject = findViewById(R.id.editTextSearchProject);
+        buttonSearchProject = findViewById(R.id.buttonSearchProject);
+        buttonBackToProfile = findViewById(R.id.buttonBackToProfile);
+
+        // Intent Factory for buttons
+        IntentFactory factory = new IntentFactory();
+
+        buttonBackToProfile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = factory.getIntent(OpenProjectsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
