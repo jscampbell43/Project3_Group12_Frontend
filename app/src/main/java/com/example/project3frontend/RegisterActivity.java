8e6cd77b0ea9dcfe3cb89ffc08f1b38ce0bee7dc
egall18 <erikgallard123@gmail.com>
2021-12-07 14:13:27 -0800

    creating register logic

--- app/src/main/java/com/example/project3frontend/RegisterActivity.java
+++ app/src/main/java/com/example/project3frontend/RegisterActivity.java
@@ -1,6 +1,11 @@
 package com.example.project3frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +15,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class RegisterActivity extends AppCompatActivity {
     // Layout pieces
     private Button buttonBackToLogin;
@@ -17,9 +24,11 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText editTextRegisterEmail;
     private EditText editTextRegisterPassword;
     private EditText editTextRegisterPasswordAgain;
+    private EditText editTextRegisterUsername;
     private Button buttonRegister;
     private static final String TAG = "RegisterActivity";
-
+    private List<User> users;
+    private Boolean check = true;
 //    private DatabaseReference mDatabase;
 //    mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -34,6 +43,7 @@ public class RegisterActivity extends AppCompatActivity {
         editTextRegisterEmail = findViewById(R.id.editTextRegisterEmail);
         editTextRegisterPassword = findViewById(R.id.editTextRegisterPassword);
         editTextRegisterPasswordAgain = findViewById(R.id.editTextRegisterPasswordAgain);
+        editTextRegisterUsername = findViewById(R.id.editTextRegisterUsername);
         buttonRegister = findViewById(R.id.buttonRegister);
 
         // Intent Factory for buttons
@@ -43,10 +53,12 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String email = editTextRegisterEmail.getText().toString();
+                String username = editTextRegisterUsername.getText().toString();
                 String password = editTextRegisterPassword.getText().toString();
                 String passwordAgain = editTextRegisterPasswordAgain.getText().toString();
+
                 // If username or password is blank
-                if(email.isEmpty() || password.isEmpty() || passwordAgain.isEmpty()) {
+                if(email.isEmpty() || username.isEmpty() || password.isEmpty() || passwordAgain.isEmpty()) {
                     Toast.makeText(RegisterActivity.this, "All fields required", Toast.LENGTH_SHORT).show();
                 // If passwords do not match
                 } else if(!password.equals(passwordAgain)){
@@ -56,8 +68,43 @@ public class RegisterActivity extends AppCompatActivity {
                     //TO DO call API to add user information to database
                     //Use intent factory to proceed to next page
                     //Use boolean statements to verify true or false for validate user and for testing purposes.
-                    Intent intent = factory.getIntent(RegisterActivity.this, LoginActivity.class);
-                    startActivity(intent);
+                    // Instance of Retrofit
+                    // Note Change .baseUrl to our heroku hosted API
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("https://shrouded-hollows-49087.herokuapp.com/")
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+                    UserApi userApi = retrofit.create(UserApi.class);
+                    Call<List<User>> call = userApi.getUsers();
+                    call.enqueue(new Callback<List<User>>(){
+                        @Override
+                        public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                            if(!response.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                            users = response.body();
+                            for (User user : users) {
+                                if(user.getEmail().equals(email)){
+                                    Toast.makeText(RegisterActivity.this, "Username already taken, try something different", Toast.LENGTH_LONG).show();
+                                    check = false;
+                                    break;
+                                }
+                            }
+                            if (check == true){
+                                //Insert new user data into database here
+                                Toast.makeText(RegisterActivity.this, "User Successfully Created", Toast.LENGTH_LONG).show();
+                                Intent intent = factory.getIntent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }else{
+                                return;
+                            }
+                        }
+                        @Override
+                        public void onFailure(Call<List<User>> call, Throwable t) {
+                            Toast.makeText(RegisterActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
             }
         });
@@ -69,5 +116,14 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private Boolean validateInput(String user){
+//        if(user.getUser_first_name().isEmpty() || usersEntity.getUser_last_name().isEmpty() || usersEntity.getUser_name().isEmpty() || usersEntity.getUser_password().isEmpty()){
+//            return false;
+//        }else{
+//            return true;
+//        }
+        return true;
     }
 }
